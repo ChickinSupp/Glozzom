44d3e3ce814225514217c38f2c13d5e18f80a7cc
O Padeiro <daviiddamiianbaker@gmail.com>
2020-05-29 17:13:59 -0400

    ekko

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,31 @@
-function getYear() {
-    $("#year").text( (new Date).getFullYear() );
-}
+//GET CURRENT YEAR
+$("#year").text(new Date().getFullYear());
 
-function carousel() {
-    $('carousel').carousel({
-        interval: 6000,
-        pause: 'hover'
-    })
-}
+// CAROUSEL SPEED
+$("carousel").carousel({
+    interval: 6000,
+    pause: "hover",
+  });
 
-function getVideo() {
-   $(function () {
-       $('.video').click(function () {
-           let theModal = $(this).data('target'),
-           videoSRC = $(this).attr('data-video'),
-           videoSRCAuto = videoSRC `watch?v=M_PwLvFDB2Y`;
-           $(theModal + ' iframe').attr('src'),videoSRCAuto;
-           $(theModal + ' button.close').click(function () {
-            $(theModal + ' iframe').attr('src'), videoSRC;
-           })
+// MODAL VIDEO ---something
+$(function () {
+    $(".video").click(function () {
+      let theModal = $(this).data("target"),
+        videoSRC = $(this).attr("data-video"),
+        videoSRCAuto =
+          videoSRC +
+          "?modestbranding=1&rel=0&controls=0&showinfo=0&html5=1&autoplay=1";
+      $(theModal + " iframe").attr("src"), videoSRCAuto;
+      $(theModal + " button.close").click(function () {
+        $(theModal + " iframe").attr("src"), videoSRC;
+      });
+    });
+  });
 
-       })
-   }) 
-}
+// EKKO LIGHTBOX
+  $(document).on('click', '[data-toggle="lightbox"]', function(event)  {
+    event.preventDefault();
+    $(this).ekkoLightbox();
+});
+
+
